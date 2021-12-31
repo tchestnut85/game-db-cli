7325a8d23d03dab245e04b38307dc0c571b19dff
Tom Chestnut <tchestnut85@gmail.com>
2021-12-31 14:50:05 -0500

    fixed / route by using fileURLToPath to get __dirname

--- src/routes/htmlRoutes/index.js
+++ src/routes/htmlRoutes/index.js
@@ -1,6 +1,10 @@
 import * as path from 'node:path';
+import { fileURLToPath } from 'url';
 import express from 'express';
+
 const router = express.Router();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 router.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '../../../static/index.html'));
