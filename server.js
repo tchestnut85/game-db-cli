10e0f46c55e178cb526229de533215b8a9028b6e
Tom Chestnut <tchestnut85@gmail.com>
2021-12-31 11:37:14 -0500

    added scripts to run the server and the cli at the same time; using exec from child_process module to open the cli in a new terminal window

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import routes from './src/routes/index.js';
 const app = express();
+import { exec } from 'node:child_process';
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,4 +12,14 @@ app.use('/', routes);
 app.use(express.static('static'));
 
 // start the server
-app.listen(PORT, () => console.log(`\n🎮 App running on port ${PORT} 🎮 🎉\n`));
+app.listen(PORT, () =>
+	console.log(`\n🎮 App running on port http://localhost:${PORT} 🎮 🎉\n`)
+);
+
+// start and open the game-database cli in a new terminal window
+const terminalExe = 'start cmd.exe /K'; // TODO - "start cmd.exe" works for windowsOS, need to check the command for macOS
+const CLI_COMMAND = `${terminalExe} game-database && npm run cli-start`;
+exec(CLI_COMMAND, (err, stdout, stderr) => {
+	if (err) console.error(`error: ${err}`);
+	if (stderr) console.error(`stderr: ${stderr}`);
+});
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
-import * as readline from 'node:readline';
+import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'process';
 
+console.log('Game Database launched 🎮\n');
 const rl = readline.createInterface({ input, output });
 
-rl.question('Hi how are you?');
+const answer1 = await rl.question('Hi how are you?');
+console.log(answer1);
